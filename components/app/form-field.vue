523873f70c1b5a79045b0f8a104eb4fd231c860f
Rajawali Ramadhan Babelian <67893489+babelianrr@users.noreply.github.com>
2025-07-07 16:56:31 +0700

    :sparkles: create new locations form (#63)

---
+++ components/app/form-field.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    label: string;
+    name: string;
+    error?: string;
+    type?: "text" | "textarea" | "number";
+    disabled?: boolean;
+}>();
+</script>
+
+<template>
+    <fieldset class="fieldset">
+        <legend class="fieldset-legend">
+            {{ props.label }}
+        </legend>
+        <Field
+            :as="props.type === 'textarea' ? 'textarea' : 'input'"
+            :name="props.name"
+            :type="props.type"
+            :disabled="disabled"
+            class="w-full"
+            :class="{
+                'input-error': props.error,
+                'input': type !== 'textarea',
+                'textarea': type === 'textarea',
+            }"
+        />
+        <p v-if="props.error" class="label text-error">
+            {{ props.error }}
+        </p>
+    </fieldset>
+</template>
