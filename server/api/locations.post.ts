7ec7c70573aff394a41587b66f5ddc2f17438ada
Rajawali Ramadhan Babelian <67893489+babelianrr@users.noreply.github.com>
2025-07-08 11:25:41 +0700

    :sparkles: validate unique slug (#64)

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,11 @@
+import { and, type DrizzleError, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
+
 import db from "../../lib/db";
 import { InsertLocation, location } from "../../lib/db/schema";
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
 
 export default defineEventHandler(async (event) => {
     if (!event.context.user) {
@@ -33,11 +39,52 @@ export default defineEventHandler(async (event) => {
         }));
     }
 
-    const [created] = await db.insert(location).values({
-        ...result.data,
-        slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
-        userId: event.context.user.id,
-    }).returning();
+    const locationExist = await db.query.location.findFirst({
+        where: and(
+            eq(location.name, result.data.name),
+            eq(location.userId, event.context.user.id),
+        ),
+    });
 
-    return created;
+    if (locationExist) {
+        return sendError(event, createError({
+            statusCode: 409,
+            statusMessage: `Location '${result.data.name}' is already exist.`,
+        }));
+    }
+
+    let slug = slugify(result.data.name);
+    let slugExist = !!(await db.query.location.findFirst({
+        where: eq(location.slug, slug),
+    }));
+
+    while (slugExist) {
+        const id = nanoid();
+        const idSlug = `${slug}-${id}`;
+        slugExist = !!(await db.query.location.findFirst({
+            where: eq(location.slug, idSlug),
+        }));
+        if (!slugExist) {
+            slug = idSlug;
+        }
+    }
+
+    try {
+        const [created] = await db.insert(location).values({
+            ...result.data,
+            slug,
+            userId: event.context.user.id,
+        }).returning();
+        return created;
+    }
+    catch (e) {
+        const error = e as DrizzleError;
+        if (error.message.includes(`Failed query`)) {
+            return sendError(event, createError({
+                statusCode: 409,
+                statusMessage: "Slug must be unique (the location name is used to generate the slug).",
+            }));
+        }
+        throw error;
+    }
 });
