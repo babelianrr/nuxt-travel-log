0811958a2152e4aaa5722125eb1518322b63fc62
Rajawali Ramadhan Babelian <67893489+babelianrr@users.noreply.github.com>
2025-08-06 16:53:54 +0700

    :sparkles: lightbox (#88)

--- components/image-list.vue
+++ components/image-list.vue
@@ -1,24 +1,45 @@
 <script lang="ts" setup>
 import type { SelectLocationLogImage } from "~/lib/db/schema";
 
-defineProps<{
+const props = defineProps<{
     images: SelectLocationLogImage[];
 }>();
 
 const config = useRuntimeConfig();
+
+const visibleRef = ref(false);
+const indexRef = ref(0);
+
+const imgs = props.images.map((v) => {
+    return `${config.public.s3BucketUrl}/${v.key}`;
+});
+
+function showImg(index: number) {
+    indexRef.value = index;
+    visibleRef.value = true;
+}
+
+const onHide = () => (visibleRef.value = false);
 </script>
 
 <template>
     <div class="flex gap-2 overflow-auto">
         <div
-            v-for="image in images"
+            v-for="(image, index) in props.images"
             :key="image.id"
             class="card card-compact h-40 w-64 shrink-0 flex items-center justify-center bg-base-300"
+            @click="() => showImg(index)"
         >
-            <div class="card-body size-full">
+            <div class="card-body size-full cursor-pointer">
                 <img class="size-full object-cover" :src="`${config.public.s3BucketUrl}/${image.key}`">
                 <slot :image />
             </div>
         </div>
+        <VueEasyLightbox
+            :visible="visibleRef"
+            :imgs="imgs"
+            :index="indexRef"
+            @hide="onHide"
+        />
     </div>
 </template>
